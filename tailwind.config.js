9f349a44aa27723e1de1e7d6ed40b9a5e0d63a27
Tharun Ravuri <tharunravuri2000@gmail.com>
2021-02-23 16:42:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': ['Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')
+,require('@tailwindcss/forms')
+,require('@tailwindcss/line-clamp')
+,require('@tailwindcss/typography')
+],
+};
